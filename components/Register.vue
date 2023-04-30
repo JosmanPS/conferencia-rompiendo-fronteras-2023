97571e9d96042fc83d6b798099cf8c02b68c25b3
Josman Proudinat <jmps2812@gmail.com>
2023-04-29 18:56:14 -0600

    Google form

--- components/Register.vue
+++ components/Register.vue
@@ -2,12 +2,20 @@
   <section id="register" class="py-24 bg-primary">
     <div class="container mx-auto">
       <div class="text-center max-w-4xl mx-auto">
-        <h2 class="text-6xl font-bold font-condensed text-canary mb-12">Regístrate</h2>
+        <h2 class="text-6xl font-bold font-condensed text-canary mb-12">
+          Regístrate
+        </h2>
         <p class="text-white font-medium text-xl mb-16">
           Déjanos tus datos y un miembro de nuetro equipo te contactará para
           darte las instrucciones de pago y completar tu registro.
         </p>
-        <div class="bg-white shadow-lg py-16 rounded-md"></div>
+        <div class="bg-white shadow-lg rounded-xl w-sm">
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdc_GdIGsuzhe5ZtG9YSPZaD0pYDByMlSFRkHFsDfCf_gvukQ/viewform?embedded=true"
+            class="w-full h-[1300px]"
+            >Cargando…</iframe
+          >
+        </div>
       </div>
     </div>
   </section>
